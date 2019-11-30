01ab01b3f53dfe4bd4b82305018cadf476a74dd8
Vilhelm Prytz <vilhelm@prytznet.se>
2019-11-30 01:04:36 +0100

    Add build testing & inital unittest structure

---
+++ tests/test_basic.py
@@ -0,0 +1,38 @@
+###########################################################
+#         _                       _                       #
+#        | |                     (_)                      #
+#   _ __ | |_ __ _ _ __ _ __ ___  _  __ _  __ _ _ __      #
+#  | '_ \| __/ _` | '__| '_ ` _ \| |/ _` |/ _` | '_ \     #
+#  | |_) | || (_| | |  | | | | | | | (_| | (_| | | | |    #
+#  | .__/ \__\__,_|_|  |_| |_| |_|_|\__, |\__,_|_| |_|    #
+#  | |                               __/ |                #
+#  |_|                              |___/                 #
+#                                                         #
+# Copyright (C) 2019, Vilhelm Prytz <vilhelm@prytznet.se> #
+# https://github.com/VilhelmPrytz/ptarmigan               #
+#                                                         #
+###########################################################
+
+# tests/test_basic.py
+
+import os
+import unittest
+ 
+from app import app
+
+class BasicTestCase(unittest.TestCase):
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+
+        self.assertEqual(response.status_code, 200)
+    
+    def test_404(self):
+        tester = app.test_client(self)
+        response = tester.get('a', content_type='html/text')
+
+        self.assertEqual(response.status_code, 404)
+        self.assertTrue(b'404' in response.data)
+
+if __name__ == "__main__":
+    unittest.main()
